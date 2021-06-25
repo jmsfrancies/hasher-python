a854add66b9dcd7fe956e0b3295b6f130336724e
James Francies <jfrancies1@live.maryville.edu>
2021-06-25 13:09:11 -0500

    updated the function name with a more user-friendly name

--- hasher.py
+++ hasher.py
@@ -1,6 +1,6 @@
 import hashlib
 
-def get_password(user_name,password):
+def hash_username_and_password(user_name,password):
     hasher_1 = hashlib.sha1()
     hasher_2 = hashlib.sha1()
     hasher_1.update(user_name)
@@ -14,7 +14,7 @@ def main():
     user_name = str(input("Please enter your preferred username: "))
     password = str(input("Please enter your password (Must be between eight to fifteen characters): "))
     if len(password) >= 8 and len(password) <= 15:
-        get_password(user_name.encode('utf-8'),password.encode('utf-8'))
+        hash_username_and_password(user_name.encode('utf-8'),password.encode('utf-8'))
         print("%s \n%s"%(user_name,password))
     else:
         print("Please enter a different password that fulfills the condition of eight to fifteen characters:")
