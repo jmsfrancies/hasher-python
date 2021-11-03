5a669532a91d009ba38672d0a846d87feab1d703
James Francies <jfrancies1@live.maryville.edu>
2021-11-03 05:28:22 -0500

    the file is opened to write only

--- hasher.py
+++ hasher.py
@@ -1,9 +1,9 @@
 import hashlib
     
-    
+#Main function that takes input added to the word-list.txt file and hashes file with SHA-512    
 def main():
     words_to_add = int(input("How many words would you like to add to the word-lists: "))
-    f = open('word-list.txt','a')
+    f = open('word-list.txt','w')
     for i in range(words_to_add):
         add_word = str(input("Add a word: ")).encode('UTF-8')
         f.write('{}'.format(add_word))
