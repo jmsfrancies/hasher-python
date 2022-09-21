c5d19d373f2f750924b54c3fef26573402605c9a
James Francies <jfrancies1@live.maryville.edu>
2022-09-21 12:37:55 -0500

    Made a change to the hasher.py function to hash statements.

--- hasher.py
+++ hasher.py
@@ -2,14 +2,13 @@ import hashlib
     
 #Main function that takes input added to the word-list.txt file and hashes file with SHA-512    
 def main():
-    words_to_add = int(input("How many words would you like to add to the word-lists: "))
+    words_to_add = str(input("Please enter a phrase to hash with SHA-512: "))
     f = open('word-list.txt','w')
-    for i in range(words_to_add):
-        add_word = str(input("Add a word: ")).encode('UTF-8')
-        f.write('{}'.format(add_word))
-        hasher = hashlib.sha512()
-        hasher.update(add_word)
-        f.write('\n{}\n'.format(hasher.digest()))
+    words_to_add = words_to_add.encode('UTF-8')
+    f.write('{}'.format(words_to_add))
+    hasher = hashlib.sha512()
+    hasher.update(words_to_add)
+    f.write('\n{}\n'.format(hasher.digest()))
         
     f.close()
                 
